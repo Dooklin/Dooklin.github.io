5aa05d222dde9d1808fdb4c61e16687baf2849a4
Dooklin <124918911+Dooklin@users.noreply.github.com>
2023-03-12 17:46:18 +0100

    Update cell.js

--- static/secretSites/moreSecret/jail/cell.js
+++ static/secretSites/moreSecret/jail/cell.js
@@ -4,9 +4,5 @@ let dip = document.getElementById("dip");
 breakOut.addEventListener("click", function guilty (){
   document.getElementById("pic").innerHTML = '<img src="../../../pics/secret/Doggosaurus.png" alt="doggo" height="25%" width="25%">';
   document.getElementById("hey").innerHTML = "HEY! Where do you think you're going?!";
-  document.getElementById("dip").innerHTML = "dip";
-});
-
-dip.addEventListener("click", function dip () {
-  location.href = "../escape.html";
+  document.getElementById("dip").innerHTML = "<h1>dip</h1>";
 });
