e971f3c01b65f89b2b7f2af890a8695f696218ed
Dooklin <124918911+Dooklin@users.noreply.github.com>
2023-04-02 11:10:02 +0200

    Delete static directory

--- static/secretSites/moreSecret/jail/cell.js
+++
@@ -1,8 +0,0 @@
-let breakOut = document.getElementById("break");
-let dip = document.getElementById("dip");
-
-breakOut.addEventListener("click", function guilty (){
-  document.getElementById("pic").innerHTML = '<img src="../../../pics/secret/Doggosaurus.png" alt="doggo" height="25%" width="25%">';
-  document.getElementById("hey").innerHTML = "HEY! Where do you think you're going?!";
-  document.getElementById("dip").innerHTML = "<h1>dip</h1>";
-});
